6bbd5a094bd6b6a9fe49062dd339e95a9bd98b32
Alex Diker <alex.diker@live.ca>
2020-08-09 10:15:49 -0400

    Add files via upload

--- input_data.py
+++ input_data.py
@@ -4,3 +4,20 @@ import sqlite3
 def c(value):
     if value.startswith("~"):
         return value.strip('~')
+    if not value:
+        value = "0"
+    return float(value)
+
+
+cc = sqlite3.connect('cars.db')
+curs = cc.cursor()
+
+curs.execute('''
+CREATE TABLE food (
+id      TEXT        PRIMERY KEY
+dec     TEXT,
+bmw     TEXT, 
+audi    TEXT,
+gas     TEXT,
+washerfluid
+''')
